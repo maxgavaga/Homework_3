23f5d3cd400aa6d4f37481a027fab05e121e3d6b
maxgavaga <ferrarif138@yandex.ru>
2022-06-22 15:03:21 +0300

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдает таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+  int length = cube.Length;
+  for (int counter = 1; counter < length; counter++)
+  {
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+  }
+}
+
+void PrintArray(int[] coll)
+{
+  int count = coll.Length;
+  for (int index = 1; index < count; index++)
+  {
+    Console.Write(coll[index]+ " ");
+  }
+} 
+
+int[] array = new int[cube+1];
+Cube(array);
+PrintArray(array);
